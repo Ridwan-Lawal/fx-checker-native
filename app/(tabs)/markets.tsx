b75370be6e7ccf51f76593239b4a9e5be9d27e4e
Ridwan <lawal6382@gmail.com>
2026-06-27 11:03:12 +0100

    feat: add the market screen functionality

--- app/(tabs)/markets.tsx
+++ app/(tabs)/markets.tsx
@@ -1,16 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { colors, fontFamily } from "@/styles/tokens";
 import { Image } from "expo-image";
 import { getFlag } from "@/utils/flags";
-
-const positiveChart = require("@/assets/charts/positive-chart.svg");
-const negativeChart = require("@/assets/charts/negative-charts.svg");
+import { CURRENCY_PAIRS } from "@/utils/contants";
+import CurrencyPairContainer from "@/components/markets/CurrencyPairContainer";
 
 export default function MarketScreen() {
-  // fetch the rates using Promise.all()
-
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -18,33 +15,15 @@ export default function MarketScreen() {
         <Text style={styles.headerSubText}>LIVE · ECB DATA</Text>
       </View>
 
-      <View style={styles.currencyPairsList}>
-        <View style={styles.currencyPairContainer}>
-          <View style={styles.firstFlexItem}>
-            <View style={styles.flags}>
-              <Image
-                source={getFlag("USD".slice(0, 2))}
-                style={styles.flagImage}
-                priority="high"
-              />
-              <Image
-                source={getFlag("USD".slice(0, 2))}
-                style={[styles.flagImage, styles.secondFlagImage]}
-                priority="high"
-              />
-            </View>
-            <Text style={styles.currencyPairTitle}>USD/GBP</Text>
-          </View>
-
-          {/*second flex item*/}
-          <Image source={positiveChart} style={styles.currencyPairChart} />
-
-          <View style={styles.thirdFlexItem}>
-            <Text style={styles.currencyPairRate}>1.1723</Text>
-            <Text style={styles.rateChange}>▲ +0.18%</Text>
-          </View>
-        </View>
-      </View>
+      <FlatList
+        data={CURRENCY_PAIRS}
+        renderItem={({ item }) => {
+          return <CurrencyPairContainer currencyPair={item} />;
+        }}
+        keyExtractor={(item) => item}
+        contentContainerStyle={{ gap: 10, ...styles.currencyPairsList }}
+      />
+      <View style={styles.currencyPairsList}></View>
     </SafeAreaView>
   );
 }
@@ -82,63 +61,5 @@ const styles = StyleSheet.create({
   currencyPairsList: {
     paddingHorizontal: 20,
     paddingVertical: 20,
-  },
-
-  currencyPairContainer: {
-    flexDirection: "row",
-    paddingHorizontal: 14,
-    paddingVertical: 13,
-    alignItems: "center",
-    gap: 12,
-    borderWidth: 1,
-    backgroundColor: colors.neutral[700],
-    borderRadius: 14,
-    borderColor: "#2a2a2e",
-    alignSelf: "stretch",
-    justifyContent: "space-between",
-  },
-  firstFlexItem: {
-    flexDirection: "row",
-    gap: 12,
-    alignItems: "center",
-  },
-  flags: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  flagImage: {
-    width: 26,
-    height: 26,
-    borderRadius: 999,
-  },
-  secondFlagImage: {
-    marginLeft: -8,
-    width: 22,
-    height: 22,
-  },
-  currencyPairTitle: {
-    color: colors.neutral[70],
-    fontFamily: fontFamily.bold,
-    fontSize: 13,
-    letterSpacing: 0.5,
-    textTransform: "uppercase",
-  },
-
-  currencyPairChart: {
-    height: 28,
-    width: 70,
-  },
-  thirdFlexItem: {
-    alignItems: "flex-end",
-  },
-  currencyPairRate: {
-    color: colors.neutral[70],
-    fontFamily: fontFamily.bold,
-    fontSize: 14,
-  },
-  rateChange: {
-    color: colors.lime[500],
-    fontFamily: fontFamily.bold,
-    fontSize: 11,
   },
 });
